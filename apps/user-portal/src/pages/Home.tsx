40e4cb4998643b48bcb680793067b4d58db3fd36
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2026-03-30 11:49:40 -0300

    Removing welcome message and initial auth information in home page

--- apps/user-portal/src/pages/Home.tsx
+++ apps/user-portal/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { HomeCreatePostSection } from '../components/home/HomeCreatePostSection';
 import { HomeFeedSection } from '../components/home/HomeFeedSection';
-import { HomeProfileSummary } from '../components/home/HomeProfileSummary';
 import { useHomeStateContract } from '../state-contracts/home';
 
 export function Home() {
@@ -8,14 +7,7 @@ export function Home() {
 
   return (
     <div data-testid="home-page" className="page-container max-w-5xl">
-      <HomeProfileSummary
-        user={state.user}
-        isLoading={state.isProfileLoading}
-        isRefreshing={state.isProfileRefreshing}
-        error={state.profileError}
-      />
-
-      <div className="mt-7">
+      <div>
         <HomeCreatePostSection onPostCreated={actions.refreshFeed} />
       </div>
 
